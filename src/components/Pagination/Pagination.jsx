d52db88776e2f4eb090b718b9e9157d75bfabb79
heneralovsem <semm297@gmail.com>
2023-08-20 14:05:08 +0300

    some fixes

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import cl from './Pagination.module.css'
+import cl from "./Pagination.module.css";
 import Icon from "@mdi/react";
 import { mdiArrowLeft, mdiArrowRight } from "@mdi/js";
 const Pagination = function (props) {
-    function splitNext() {
-        props.setSplit(props.split + 8)
-      }
-      function splitPrev() {
-        props.setSplit(props.split - 8)
-      }  
-    
-    return (
-        <div className={cl.pagination}>
-            {props.split > 0 ?  <button className={cl.prevbtn} onClick={splitPrev}><Icon path={mdiArrowLeft} size={1}/></button> : null}
-            {props.split < 32 ? <button className={cl.nextbtn} onClick={splitNext}><Icon path={mdiArrowRight} size={1}/></button> : null}
-    
-       
-        </div>
-        
-    );
+  function splitNext() {
+    props.setSplit(props.split + 8);
+  }
+  function splitPrev() {
+    props.setSplit(props.split - 8);
+  }
+
+  return (
+    <div className={cl.pagination}>
+      {props.split > 0 ? (
+        <button className={cl.prevbtn} onClick={splitPrev}>
+          <Icon path={mdiArrowLeft} size={1} />
+        </button>
+      ) : null}
+      {props.split < 32 ? (
+        <button className={cl.nextbtn} onClick={splitNext}>
+          <Icon path={mdiArrowRight} size={1} />
+        </button>
+      ) : null}
+    </div>
+  );
 };
 export default Pagination;
--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,50 +1,71 @@
 import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../context";
-import Button from '@mui/material/Button'
-import cl from './Navbar.module.css'
+import Button from "@mui/material/Button";
+import cl from "./Navbar.module.css";
 
 const Navbar = () => {
-    const {isAuth, setIsAuth} = useContext(AuthContext)
-    const [openBurger, setOpenBurger] = useState(false)
-    let navUser = localStorage.getItem('name')
-    const logout = () => {
-        setIsAuth(false);
-        localStorage.removeItem('auth')
-        localStorage.removeItem('name')
-      }
-    const openNav = () => {
-        setOpenBurger(!openBurger)
-    }
-    return (
-        <div className={cl.menu__bg}>
-        <div className={cl.menu}>
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+  const [openBurger, setOpenBurger] = useState(false);
+  let navUser = localStorage.getItem("name");
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.removeItem("auth");
+    localStorage.removeItem("name");
+  };
+  const openNav = () => {
+    setOpenBurger(!openBurger);
+  };
+  return (
+    <div className={cl.menu__bg}>
+      <div className={cl.menu}>
         <div className={cl.menu__links}>
-        <Link className={cl.menu__link} to="/about">About</Link>
-        <Link className={cl.menu__link} to="/weather">Weather</Link>
-        <Link className={cl.menu__link} to="/profile">Profile</Link>
+          <Link className={cl.menu__link} to="/about">
+            About
+          </Link>
+          <Link className={cl.menu__link} to="/weather">
+            Weather
+          </Link>
+          <Link className={cl.menu__link} to="/profile">
+            Profile
+          </Link>
         </div>
         <div onClick={openNav} className={cl.burger__wrapper}>
-        <span className={cl.bar}></span>
-        <span className={cl.bar}></span>
-        <span className={cl.bar}></span>
-        {openBurger ? <div className={cl.burger__links}>
-        <Link className={cl.menu__link} to="/about">About</Link>
-        <Link className={cl.menu__link} to="/weather">Weather</Link>
-        <Link className={cl.menu__link} to="/profile">Profile</Link>
-            </div> : null }
+          <span className={cl.bar}></span>
+          <span className={cl.bar}></span>
+          <span className={cl.bar}></span>
+          {openBurger ? (
+            <div className={cl.burger__links}>
+              <Link className={cl.menu__link} to="/about">
+                About
+              </Link>
+              <Link className={cl.menu__link} to="/weather">
+                Weather
+              </Link>
+              <Link className={cl.menu__link} to="/profile">
+                Profile
+              </Link>
+            </div>
+          ) : null}
         </div>
-        
+
         <div className={cl.auth__links}>
-        {isAuth ? <span className={cl.username}>{navUser}</span> : null }
-        {!isAuth ? <Button variant="outlined"><Link className={cl.button__link} to="/login">Log in</Link></Button> : <Button variant="outlined" onClick={logout}>Log out</Button> }
-      
+          {isAuth ? <span className={cl.username}>{navUser}</span> : null}
+          {!isAuth ? (
+            <Button variant="outlined">
+              <Link className={cl.button__link} to="/login">
+                Log in
+              </Link>
+            </Button>
+          ) : (
+            <Button variant="outlined" onClick={logout}>
+              Log out
+            </Button>
+          )}
         </div>
-        
-     
+      </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Navbar;
